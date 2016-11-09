10d4c532264ecea75c6faa492fdf3a6d4e6e42a0
MinerBigWhale <cedricbaleine@gmail.com>
2016-11-09 18:28:24 +0100

    Automatically fill credential into correct field

---
+++ background.js
@@ -0,0 +1,6 @@
+(function() {
+  chrome.storage.sync.get(['username', 'password', 'auto'], function(items) {
+    document.querySelector("input[type='text'][name='username']").value = items.username;
+    document.querySelector("input[type='password'][name='password']").value = items.password;
+  });
+})();
