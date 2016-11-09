747c2436d065fea2b2e70feb3fdc28953438612a
MinerBigWhale <cedricbaleine@gmail.com>
2016-11-09 18:32:13 +0100

    Add Auto Click on submit button

--- background.js
+++ background.js
@@ -2,5 +2,8 @@
   chrome.storage.sync.get(['username', 'password', 'auto'], function(items) {
     document.querySelector("input[type='text'][name='username']").value = items.username;
     document.querySelector("input[type='password'][name='password']").value = items.password;
+    if (items.auto){
+      document.querySelector("input[type='submit'][name='submit']").click();
+    }
   });
 })();
